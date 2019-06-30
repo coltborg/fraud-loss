9acc3e5e2c46bce34f38a4414f9849ba5bc1afde
coltborg <coltborg@gmail.com>
2019-06-30 17:53:54 -0500

    Add data and graph

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
-  <div id="app" class>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div
+    id="app"
+    class="p-4 md:p-8 min-h-screen bg-gray-100 text-gray-800 font-sans antialiased"
+  >
+    <h1
+      class="mb-4 font-semibold text-2xl md:text-3xl uppercase leading-tight tracking-wide"
+    >
+      Fraud Loss
+    </h1>
+    <Card />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Card from "./components/Card.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Card
   }
 };
 </script>
-
-<style></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="p-8 rounded-lg bg-white shadow-lg">
+    <div v-if="!loaded" class="p-6 text-xl text-gray-600 font-bold text-center">
+      Loading...
+    </div>
+    <Chart v-if="loaded" :chartData="chartData" :options="options" />
+  </div>
+</template>
+
+<script>
+import Chart from "./Chart.vue";
+import data from "../data/data.js";
+
+export default {
+  name: "Card",
+  components: {
+    Chart
+  },
+  data: () => ({
+    loaded: false,
+    chartData: null,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Months"
+            }
+          }
+        ],
+        yAxes: [
+          {
+            id: "cards-y-axis",
+            scaleLabel: {
+              display: true,
+              labelString: "Amount of Active Cards",
+              fontColor: "#285e61"
+            }
+          },
+          {
+            id: "fraud-y-axis",
+            scaleLabel: {
+              display: true,
+              labelString: "Amount of Money Lost ($)",
+              fontColor: "#9b2c2c"
+            }
+          }
+        ]
+      }
+    }
+  }),
+  mounted() {
+    this.loadChartData();
+  },
+  methods: {
+    loadChartData() {
+      this.loaded = false;
+      setTimeout(() => {
+        this.chartData = data;
+        this.loaded = true;
+      }, 500);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
